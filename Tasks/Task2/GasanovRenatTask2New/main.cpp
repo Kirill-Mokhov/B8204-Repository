42dae44fe97840c5100d882fe5cabe3181bda905
mimiMileena <ren99999@mail.ru>
2018-03-20 01:11:32 +1000

    task 2 new 01:11

---
+++ Tasks/Task2/GasanovRenatTask2New/main.cpp
@@ -0,0 +1,23 @@
+#include "matrix_lib.h"
+#include <conio.h>
+#include <ctime>
+using namespace std;
+using MatrixLib::Matrix;
+
+int main()
+{
+	srand(time(NULL));
+	Matrix a(3, 2);
+	Matrix b(2, 3);
+	cout << (a);
+	cout << (b);
+	cout << a + b;
+	cout << a - b;
+	cout << a * b;
+	cout << 5 * a;
+	cout << a * 5;
+	Matrix c(3, 3);
+	cout << c.getInverse() << endl;
+	cout << c.getDeterminant() << endl;
+	_getch();
+}
---
+++ Tasks/Task2/GasanovRenatTask2New/matrix_lib_t.hpp
@@ -0,0 +1,20 @@
+#ifndef PROJECT_MATRIX_LIB_T_H
+#define PROJECT_MATRIX_LIB_T_H
+template<typename T>
+MatrixLib::Matrix MatrixLib::operator*(const MatrixLib::Matrix &a, const T &multiplier) {
+
+	Matrix result(a);
+	for (MSIZE_T i(0); i < a.m; i++)
+	{ for (MSIZE_T j(0); j < a.n; j++) {result[i][j] *= multiplier;} }
+	return result;
+}
+
+template<typename T>
+MatrixLib::Matrix MatrixLib::operator*(const T &multiplier, const MatrixLib::Matrix &a) {
+
+	Matrix result(a);
+	for (MSIZE_T i(0); i < a.m; i++)
+	{ for (MSIZE_T j(0); j < a.n; j++) {result[i][j] *= multiplier;} }
+	return result;
+}
+#endif //PROJECT_MATRIX_LIB_T_H
